e8fcc2a6b308f0dc6def0a79da80cb51f09a0b95
teraomoyu <dmntc.5299@gmail.com>
2022-06-22 01:14:02 +0900

    fix: 解答一覧を見るページ

--- front/react/src/pages/AnswerList/AnswerList.jsx
+++ front/react/src/pages/AnswerList/AnswerList.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import Dummy from './dummy.json'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const AnswerList = () => <ul>
-    {Dummy.map(item => (
-        <li key={item}>Q{item.question_id} {item.question} <input type="button" value="見る" /></li>
-    ))}
-</ul>
+import { Button } from '@mui/material'
+import style from './AnswerList.module.scss'
+
+const AnswerList = () => {
+  const questions = [
+    { question_id: 0, question: "どんな人？" },
+    { question_id: 1, question: "あだ名は？" },
+    { question_id: 2, question: "あだ名は？" },
+    { question_id: 3, question: "どんな人？" },
+    { question_id: 4, question: "どんな人？" }
+  ]
+
+  const [setCurrentQuestion] = useState(questions[0])
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0)
+  const [setCurrentAnswer] = useState('')
+  const [answerList, setAnswerList] = useState([])
+
+  const navigate = useNavigate()
+
+  const popFromAnswerList = () => {
+    const newAnswerList = [...answerList]
+    newAnswerList.pop()
+    setAnswerList(newAnswerList)
+    setCurrentAnswer('')
+  }
+
+  const clickPrevButton = () => {
+    if (currentQuestionNumber === 1) navigate('/select-person')
+    else {
+      setCurrentQuestion(questions[currentQuestionNumber - 1])
+      setCurrentQuestionNumber(currentQuestionNumber - 1)
+      popFromAnswerList()
+    }
+  }
+
+  return (
+    <div className={style.container}>
+      <h1>解答一覧を見る画面</h1>
+      <div className={style.personImage}>image</div>
+      <ul>
+        {questions.map(item => (
+            <li key={item}>Q{item.question_id+1} {item.question} <Button onClick={clickPrevButton} variant="outlined">見る</Button></li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default AnswerList
--- front/react/src/pages/SelectPerson/SelectPerson.jsx
+++ front/react/src/pages/SelectPerson/SelectPerson.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Button from '@mui/material/Button'
+
+import style from './SelectPerson.module.scss'
 
 const SelectPerson = () => {
-  const getTargetPictures = () => ['pic1', 'pic2', 'pic3']
+  const persons = [
+    { targetid: 'id1', picture: 'picture1', name: 'test1' },
+    { targetid: 'id2', picture: 'picture2', name: 'test2' },
+    { targetid: 'id3', picture: 'picture3', name: 'test3' },
+  ]
 
-  const pictures = getTargetPictures()
+  const navigate = useNavigate()
+
+  const clickAnswerQuestionButton = (targetid) => {
+    // 質問を答える画面へ遷移する関数
+    // stateにtargetid格納し，遷移先の画面へ値を渡す
+    navigate('/question-list', { state: { targetid } })
+  }
+  const clickSeeAnswerButton = (targetid) => {
+    // 回答を見る画面へ遷移する関数
+    // stateにtargetid格納し，遷移先の画面へ値を渡す
+    navigate('/answer-list', { state: { targetid } })
+  }
 
   return (
-    <div>
-      <h1>SelectPerson</h1>
-      <ul>
-        {pictures.map((picture) => (
-          <li key={picture}>{picture}</li>
+    <div className={style.container}>
+      <h1>対象の人を選ぶ画面</h1>
+      <div className={style.personContainer}>
+        {persons.map((person) => (
+          <div className={style.personRow} key={person.targetid}>
+            <div className="person-image">{person.picture}</div>
+            {/* TODO: divからbase64形式の画像表示に変更 */}
+            <Button onClick={() => clickAnswerQuestionButton(person.targetid)} variant="outlined">
+              回答する
+            </Button>
+            <Button onClick={() => clickSeeAnswerButton(person.targetid)} variant="outlined">
+              回答を見る
+            </Button>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }

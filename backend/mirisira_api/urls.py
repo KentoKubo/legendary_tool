ffa569d4a24a4feb580100036e9437ab6f71e3a5
nono927 <n_ono@hal.t.u-tokyo.ac.jp>
2022-06-23 11:40:43 +0900

    fix: test APIのパスを修正

--- backend/mirisira_api/urls.py
+++ backend/mirisira_api/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from mirisira_api.views import *
 
 urlpatterns = [
-    url(r'^test$', APITestView.as_view()),
+    url('test/', APITestView.as_view()),
     url('targets/', TargetList.as_view()),
     url('questions/', QuestionList.as_view()),
     url('answers/', AnswerList.as_view()),

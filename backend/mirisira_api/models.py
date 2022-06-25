2a39d29d9f916ee6ae8af038feb7fcbaebf7da8b
KentoKubo <kento.kubo@hamadalab.com>
2022-06-25 18:41:42 +0900

    feat: add models

--- backend/mirisira_api/models.py
+++ backend/mirisira_api/models.py
@@ -1,11 +1,30 @@
+from xml.etree.ElementTree import PI
 from django.db import models
 
 # Create your models here.
 
+class Question(models.Model):
+    title = models.CharField(max_length=31)
+    creator_name = models.CharField(max_length=31)
+    create_at = models.DateTimeField(auto_now_add=True)
 
 
+class Picture(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    file_path = models.CharField(max_length=255) # TODO:survey
 
 
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answerer_name = models.CharField(max_length=31)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class OneCharacterAnswer(models.Model):
+    picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    character_name = models.CharField(max_length=31)
+    character_explanation = models.TextField()
 
 
 # feat/2/define-model

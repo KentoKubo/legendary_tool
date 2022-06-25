bb16606be627727e90204731730a8c4937539b6c
KentoKubo <kento.kubo@hamadalab.com>
2022-06-25 19:30:57 +0900

    feat: add admin page

--- backend/mirisira_api/admin.py
+++ backend/mirisira_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Question)
+admin.site.register(Picture)
+admin.site.register(Answer)
+admin.site.register(OneCharacterAnswer)
+
+#TODO: update admin site like https://docs.djangoproject.com/ja/4.0/intro/tutorial07/

ea047f075cce5899b71c7a8faed8417e232b2b72
hi1rayama <hi1rayama@gmail.com>
2022-06-28 16:30:35 +0900

    feat: questions API POST時にmultipart/form-dataでのデータ受け取りの実装

--- backend/mirisira_api/serializers/question_serializers.py
+++ backend/mirisira_api/serializers/question_serializers.py
@@ -7,15 +7,18 @@ from mirisira_api.models import Question, Picture
 from drf_extra_fields.fields import Base64ImageField
 
 class PictureSerializer(serializers.ModelSerializer):
-    picture_id = serializers.IntegerField(source="id", read_only=True)
-    picture = Base64ImageField(source="file_path",represent_in_base64=True,read_only=True)
+    picture_id = serializers.IntegerField(source="id", read_only=False)
+    picture = Base64ImageField(source="file_path",represent_in_base64=True,read_only=False)
     class Meta:
         model = Picture
         fields = ["picture_id","picture"]
 
     def create(self, validated_data):
-        file_path = Base64ImageField().to_internal_value(validated_data.pop('picture'))
-        Picture.objects.create(question=validated_data.pop('question'),file_path= file_path)
+        picture = validated_data.pop('picture')
+        if type(picture) is str:
+            picture = Base64ImageField().to_internal_value(picture)
+
+        Picture.objects.create(question=validated_data.pop('question'),file_path= picture)
         # return image
 class QuestionGetSerializer(serializers.Serializer):
     question_id = serializers.IntegerField()
--- backend/mirisira_api/views/question_views.py
+++ backend/mirisira_api/views/question_views.py
@@ -40,15 +40,26 @@ class QuestionList(generics.ListCreateAPIView):
 
 
     def post(self, request, *args, **kwargs):
-        pictures = request.data.pop('pictures')
-        question_serializer = QuestionPostSerializer(data=request.data)
+        if request.content_type == 'application/json':
+            pictures = request.data.pop('pictures')
+            question_info = request.data
+            question_serializer = QuestionPostSerializer(data=question_info)
+        else:
+            question_info = {
+                'title':request.POST.get('title',None),
+                'creator_name':request.POST.get('creator_name',None),
+            }
+            pictures = request.FILES.getlist('pictures',None)
+            question_serializer = QuestionPostSerializer(data=question_info)
+            
         with transaction.atomic():
             if question_serializer.is_valid():
-                data = question_serializer.create(request.data)
+                data = question_serializer.create(question_info)
 
             pictures_serializer = PictureSerializer(data=[{'question':data,'picture':picture} for picture in pictures],many=True)
             if pictures_serializer.is_valid():
-                pictures_serializer.create([{'question':data,'picture':picture} for picture in pictures])
+                pictures_serializer.save()
+                # pictures_serializer.create([{'question':data,'picture':picture} for picture in pictures])
 
         return JsonResponse({}, status=status.HTTP_201_CREATED)
 

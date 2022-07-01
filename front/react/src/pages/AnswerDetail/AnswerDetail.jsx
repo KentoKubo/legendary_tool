2ff9f203d72b973cebf697a5296b18b87ceaa73d
teraomoyu <dmntc.5299@gmail.com>
2022-07-01 16:35:30 +0900

    add: apiの操作

--- front/react/src/pages/AnswerDetail/AnswerDetail.jsx
+++ front/react/src/pages/AnswerDetail/AnswerDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material'
+import axios from 'axios'
 
 import Image from 'mui-image'
 import style from './AnswerDetail.module.scss'
@@ -17,84 +18,84 @@ import right from '../../images/right.png'
 const AnswerDetail = () => {
   // const answer = answers[selectedAnswerNum].answer_id
 
-  const answers1 = [
-    {
-      answer_id: 0,
-      answerer_name: 'hira',
-      create_at: '2022-06-28T06:47:20.347612Z',
-    },
-    {
-      answer_id: 1,
-      answerer_name: 'hira',
-      create_at: '2022-06-28T06:47:20.347612Z',
-    },
-  ]
+  // const answers1 = [
+  //   {
+  //     answer_id: 0,
+  //     answerer_name: 'hira',
+  //     create_at: '2022-06-28T06:47:20.347612Z',
+  //   },
+  //   {
+  //     answer_id: 1,
+  //     answerer_name: 'hira',
+  //     create_at: '2022-06-28T06:47:20.347612Z',
+  //   },
+  // ]
 
-  const answer1 = [
-    {
-      picture_id: 0,
-      character_name: '炭治郎',
-      character_explanation: '優しい人で強い',
-    },
-    {
-      picture_id: 1,
-      character_name: 'nezuko',
-      character_explanation: 'かわいい',
-    },
-    {
-      picture_id: 2,
-      character_name: 'zennitsu',
-      character_explanation: '黄色い',
-    },
-    {
-      picture_id: 3,
-      character_name: 'inosuke',
-      character_explanation: '猪',
-    },
-    {
-      picture_id: 4,
-      character_name: 'shinobu',
-      character_explanation: '怖い',
-    },
-    {
-      picture_id: 5,
-      character_name: 'giyu',
-      character_explanation: '無口',
-    },
-  ]
+  // const answer1 = [
+  //   {
+  //     picture_id: 0,
+  //     character_name: '炭治郎',
+  //     character_explanation: '優しい人で強い',
+  //   },
+  //   {
+  //     picture_id: 1,
+  //     character_name: 'nezuko',
+  //     character_explanation: 'かわいい',
+  //   },
+  //   {
+  //     picture_id: 2,
+  //     character_name: 'zennitsu',
+  //     character_explanation: '黄色い',
+  //   },
+  //   {
+  //     picture_id: 3,
+  //     character_name: 'inosuke',
+  //     character_explanation: '猪',
+  //   },
+  //   {
+  //     picture_id: 4,
+  //     character_name: 'shinobu',
+  //     character_explanation: '怖い',
+  //   },
+  //   {
+  //     picture_id: 5,
+  //     character_name: 'giyu',
+  //     character_explanation: '無口',
+  //   },
+  // ]
 
-  const answer2 = [
-    {
-      picture_id: 0,
-      character_name: '炭治郎',
-      character_explanation: '111111111122222222223333333333',
-    },
-    {
-      picture_id: 1,
-      character_name: 'nezuko',
-      character_explanation: '2',
-    },
-    {
-      picture_id: 2,
-      character_name: 'zennitsu',
-      character_explanation: '2',
-    },
-    {
-      picture_id: 3,
-      character_name: 'inosuke',
-      character_explanation: '2',
-    },
-    {
-      picture_id: 4,
-      character_name: 'shinobu',
-      character_explanation: '2',
-    },
-    {
-      picture_id: 5,
-      character_name: 'giyu',
-      character_explanation: '2',
-    },
-  ]
+  // const answer2 = [
+  //   {
+  //     picture_id: 0,
+  //     character_name: '炭治郎',
+  //     character_explanation: '111111111122222222223333333333',
+  //   },
+  //   {
+  //     picture_id: 1,
+  //     character_name: 'nezuko',
+  //     character_explanation: '2',
+  //   },
+  //   {
+  //     picture_id: 2,
+  //     character_name: 'zennitsu',
+  //     character_explanation: '2',
+  //   },
+  //   {
+  //     picture_id: 3,
+  //     character_name: 'inosuke',
+  //     character_explanation: '2',
+  //   },
+  //   {
+  //     picture_id: 4,
+  //     character_name: 'shinobu',
+  //     character_explanation: '2',
+  //   },
+  //   {
+  //     picture_id: 5,
+  //     character_name: 'giyu',
+  //     character_explanation: '2',
+  //   },
+  // ]
 
   const [selectedAnswerNum, setSelectedAnswerNum] = useState(0)
   const [answerList, setAnswerList] = useState([])
@@ -105,22 +106,22 @@ const AnswerDetail = () => {
 
   const { questionItem } = location.state
 
+
   useEffect(() => {
-    // const fetchAnswerList = async () => {
-    //   const res = await axios.get('/answers', { param: { question_id: questionItem.question_id } })
-    //   setAnswerList(res.data)
-    // }
-    // fetchAnswerList()
-    setAnswerList(answers1)
+    const getAnswerList = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/`, { param: { question_id: questionItem.question_id } })
+      setAnswerList(result.data.answers)
+    }
+    getAnswerList()
   }, [])
 
   useEffect(() => {
-    // const fetchAnswer = async () => {
-    //   const res = await axios.get('/answers/${answerList[selectedAnswerNum].answer_id}')
-    //   setAnswer(res.data)
-    // }
-    // fetchAnswer()
-    setAnswer(answer1)
+    const fetchAnswer = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
+      setAnswer(result.data.characters)
+    }
+    fetchAnswer()
+    // setAnswer(answer1)
   }, [answerList])
 
   questionItem.pictures.map((quesItem) =>
@@ -132,18 +133,16 @@ const AnswerDetail = () => {
   }
 
   const clickPrevButton = () => {
-    // if (selectedAnswerNum > 0) setSelectedAnswerNum(selectedAnswerNum - 1)
     if (selectedAnswerNum > 0) {
       setSelectedAnswerNum(selectedAnswerNum - 1)
-      setAnswer(answer1)
+      setAnswer(answerList[selectedAnswerNum])
     }
   }
 
   const clickNextButton = () => {
-    // if (selectedAnswerNum < answerList.length) setSelectedAnswerNum(selectedAnswerNum + 1)
     if (selectedAnswerNum < answerList.length) {
       setSelectedAnswerNum(selectedAnswerNum + 1)
-      setAnswer(answer2)
+      setAnswer(answerList[selectedAnswerNum])
     }
   }
 
@@ -151,10 +150,16 @@ const AnswerDetail = () => {
     <div className={style.container}>
       <i className="icon-download" style={{ fontSize: '80px' }} />
       <div style={{ display: 'flex' }}>
-        <button type="button" onClick={clickPrevButton}>
-          <img src={left} alt="previous" />
-          <Text text="前の回答へ" style={{ color: '#545454', fontSize: 'small' }} />
-        </button>
+        {(selectedAnswerNum !== 0)
+          ? <button className={style.button} type='button' onClick={clickPrevButton}>
+              <img src={left} alt="previous"/>
+              <Text text='前の回答へ' style={{color: '#545454', fontSize: 'small'}}/>
+            </button>
+          : <button type='button' onClick={clickPrevButton}>
+              <img src={left} alt="previous" style={{opacity: "50%"}}/>
+              <Text text='前の回答へ' style={{color: '#545454', fontSize: 'small', opacity: "50%"}}/>
+            </button>
+        }
         <ImageList
           cols={3}
           gap={0}
@@ -189,10 +194,16 @@ const AnswerDetail = () => {
               </ImageListItem>
             ))}
         </ImageList>
-        <button type="button" onClick={clickNextButton}>
-          <img src={right} alt="next" />
-          <Text text="次の回答へ" style={{ color: '#545454', fontSize: 'small' }} />
-        </button>
+        {(selectedAnswerNum !== 0)
+          ? <button type="button" onClick={clickNextButton}>
+              <img src={right} alt="next" />
+              <Text text="次の回答へ" style={{ color: '#545454', fontSize: 'small' }} />
+            </button>
+          : <button type="button" onClick={clickNextButton}>
+              <img src={right} alt="next" style={{opacity: "50%"}}/>
+              <Text text="次の回答へ" style={{ color: '#545454', fontSize: 'small', opacity: "50%"}} />
+            </button>
+        }
       </div>
       <FlatButton text="一覧に戻る" onClick={clickBackButton} variant="white" />
     </div>

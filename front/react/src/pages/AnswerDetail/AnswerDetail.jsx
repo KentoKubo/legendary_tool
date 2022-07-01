2d703df5a57619a305b5acdaa3dab1578723e241
teraomoyu <dmntc.5299@gmail.com>
2022-07-01 18:33:36 +0900

    feat:回答閲覧画面

--- front/react/src/pages/AnswerDetail/AnswerDetail.jsx
+++ front/react/src/pages/AnswerDetail/AnswerDetail.jsx
@@ -33,6 +33,7 @@ const AnswerDetail = () => {
     }
     getAnswerList()
   }, [])
+  console.log(answerList)
   
   useEffect(() => {
     const fetchAnswer = async () => {
@@ -40,30 +41,36 @@ const AnswerDetail = () => {
       setAnswer(result.data.characters)
     }
     fetchAnswer()
+    questionItem.pictures.map((quesItem) =>
+      answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (
+        (ansItem.picture = quesItem.picture)
+        (ansItem.answerer_name = quesItem.answerer_name)
+        (ansItem.create_at = quesItem.create_at)
+      ))
+    )
   }, [answerList])
-
-  const getAnswer = () => useEffect(() => {
-    const fetchAnswer = async () => {
-      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
-      setAnswer(result.data.characters)
-    }
-    fetchAnswer()
-  }, [answerList])
-  console.log(answerList)
-  console.log(answer)
-  console.log(selectedAnswerNum)
+  console.log("a", selectedAnswerNum)
+  console.log("name", answer.answerer_name)
+  console.log("creat", answer.create_at)
 
   const clickBackButton = () => {
     navigate(-2)
   }
 
   const clickPrevButton = () => {
-    if (selectedAnswerNum > 0) {
+    if (selectedAnswerNum >= 0) {
       setSelectedAnswerNum(selectedAnswerNum - 1)
-      getAnswer()
-      // setAnswer(answerList[selectedAnswerNum])
+      const fetchAnswer = async () => {
+        const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
+        setAnswer(result.data.characters)
+      }
+      fetchAnswer()
       questionItem.pictures.map((quesItem) =>
-        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (ansItem.picture = quesItem.picture))
+        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (
+          (ansItem.picture = quesItem.picture)
+          (ansItem.answerer_name = quesItem.answerer_name)
+          (ansItem.create_at = quesItem.create_at)
+        ))
       )
     }
   }
@@ -71,12 +78,22 @@ const AnswerDetail = () => {
   const clickNextButton = () => {
     if (selectedAnswerNum < answerList.length) {
       setSelectedAnswerNum(selectedAnswerNum + 1)
-      getAnswer()
-      // setAnswer(answerList[selectedAnswerNum])
+      const fetchAnswer = async () => {
+        const result2 = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
+        setAnswer(result2.data.characters)
+      }
+      fetchAnswer()
       questionItem.pictures.map((quesItem) =>
-        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (ansItem.picture = quesItem.picture))
+        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (
+          (ansItem.picture = quesItem.picture)
+          (ansItem.answerer_name = quesItem.answerer_name)
+          (ansItem.create_at = quesItem.create_at)
+        ))
       )
     }
+    console.log("next-answernum", selectedAnswerNum)
+    console.log("next-ansid", answerList[selectedAnswerNum].answer_id - 1)
+    console.log("next-ansname", answerList[selectedAnswerNum].answerer_name)
   }
 
   return (
@@ -93,40 +110,43 @@ const AnswerDetail = () => {
               <Text text='前の回答へ' style={{color: '#545454', fontSize: 'small', opacity: "50%"}}/>
             </button>
         }
-        <ImageList
-          cols={3}
-          gap={0}
-          sx={{
-            border: '3px solid #545454',
-            borderRadius: '4px',
-            width: '70%',
-            margin: 'auto',
-            boxShadow: '0 0 25px 0 rgba(0, 0, 0, .3)',
-            backgroundColor: '#fff',
-            '::-webkit-scrollbar': { display: 'none' },
-          }}
-        >
-          {answer.length > 0 &&
-            questionItem.pictures.map((picture, idx) => (
-              <ImageListItem className={style.imgListItem} key={picture.picture_id} sx={{ margin: '0 0 -1px -1px' }}>
-                <Image className={style.imgList} src={`data:image/png;base64,${picture.picture}`} style={{ width: '50%', margin: 'auto' }} />
-                <ImageListItemBar
-                  position="below"
-                  title={answer[idx].character_name}
-                  sx={{
-                    borderBottom: '1px solid #545454',
-                    borderTop: '1px solid #545454',
-                    height: '2em',
-                  }}
-                />
-                <ImageListItemBar
-                  position="below"
-                  title={answer[idx].character_explanation}
-                  sx={{ borderBottom: '1px solid #545454', height: '5em', lineHeight: '2em' }}
-                />
-              </ImageListItem>
-            ))}
-        </ImageList>
+        <div>
+          <ImageList
+            cols={3}
+            gap={0}
+            sx={{
+              border: '3px solid #545454',
+              borderRadius: '4px',
+              width: '70%',
+              margin: 'auto',
+              boxShadow: '0 0 25px 0 rgba(0, 0, 0, .3)',
+              backgroundColor: '#fff',
+              '::-webkit-scrollbar': { display: 'none' },
+            }}
+          >
+            {answer.length > 0 &&
+              questionItem.pictures.map((picture, idx) => (
+                <ImageListItem className={style.imgListItem} key={picture.picture_id} sx={{ margin: '0 0 -1px -1px' }}>
+                  <Image className={style.imgList} src={`data:image/png;base64,${picture.picture}`} style={{ width: '50%', margin: 'auto' }} />
+                  <ImageListItemBar
+                    position="below"
+                    title={answer[idx].character_name}
+                    sx={{
+                      borderBottom: '1px solid #545454',
+                      borderTop: '1px solid #545454',
+                      height: '2em',
+                    }}
+                  />
+                  <ImageListItemBar
+                    position="below"
+                    title={answer[idx].character_explanation}
+                    sx={{ borderBottom: '1px solid #545454', height: '5em', lineHeight: '2em' }}
+                  />
+                </ImageListItem>
+              ))}
+          </ImageList>
+          <Text text={`回答者 : ${answer.answerer_name} 回答日時 : ${answer.create_at}`} />
+        </div>
         {(selectedAnswerNum !== answerList.length - 1)
           ? <button type="button" onClick={clickNextButton}>
               <img src={right} alt="next" />

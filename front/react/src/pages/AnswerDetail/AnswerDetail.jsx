5bd908b3a963581828c11d2f79ad9a76cf59051b
teraomoyu <dmntc.5299@gmail.com>
2022-07-01 17:12:05 +0900

    commit

--- front/react/src/pages/AnswerDetail/AnswerDetail.jsx
+++ front/react/src/pages/AnswerDetail/AnswerDetail.jsx
@@ -16,86 +16,6 @@ import right from '../../images/right.png'
 /* eslint no-param-reassign:0 */
 
 const AnswerDetail = () => {
-  // const answer = answers[selectedAnswerNum].answer_id
-
-  // const answers1 = [
-  //   {
-  //     answer_id: 0,
-  //     answerer_name: 'hira',
-  //     create_at: '2022-06-28T06:47:20.347612Z',
-  //   },
-  //   {
-  //     answer_id: 1,
-  //     answerer_name: 'hira',
-  //     create_at: '2022-06-28T06:47:20.347612Z',
-  //   },
-  // ]
-
-  // const answer1 = [
-  //   {
-  //     picture_id: 0,
-  //     character_name: '炭治郎',
-  //     character_explanation: '優しい人で強い',
-  //   },
-  //   {
-  //     picture_id: 1,
-  //     character_name: 'nezuko',
-  //     character_explanation: 'かわいい',
-  //   },
-  //   {
-  //     picture_id: 2,
-  //     character_name: 'zennitsu',
-  //     character_explanation: '黄色い',
-  //   },
-  //   {
-  //     picture_id: 3,
-  //     character_name: 'inosuke',
-  //     character_explanation: '猪',
-  //   },
-  //   {
-  //     picture_id: 4,
-  //     character_name: 'shinobu',
-  //     character_explanation: '怖い',
-  //   },
-  //   {
-  //     picture_id: 5,
-  //     character_name: 'giyu',
-  //     character_explanation: '無口',
-  //   },
-  // ]
-
-  // const answer2 = [
-  //   {
-  //     picture_id: 0,
-  //     character_name: '炭治郎',
-  //     character_explanation: '111111111122222222223333333333',
-  //   },
-  //   {
-  //     picture_id: 1,
-  //     character_name: 'nezuko',
-  //     character_explanation: '2',
-  //   },
-  //   {
-  //     picture_id: 2,
-  //     character_name: 'zennitsu',
-  //     character_explanation: '2',
-  //   },
-  //   {
-  //     picture_id: 3,
-  //     character_name: 'inosuke',
-  //     character_explanation: '2',
-  //   },
-  //   {
-  //     picture_id: 4,
-  //     character_name: 'shinobu',
-  //     character_explanation: '2',
-  //   },
-  //   {
-  //     picture_id: 5,
-  //     character_name: 'giyu',
-  //     character_explanation: '2',
-  //   },
-  // ]
 
   const [selectedAnswerNum, setSelectedAnswerNum] = useState(0)
   const [answerList, setAnswerList] = useState([])
@@ -106,27 +26,32 @@ const AnswerDetail = () => {
 
   const { questionItem } = location.state
 
-
   useEffect(() => {
     const getAnswerList = async () => {
-      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/`, { param: { question_id: questionItem.question_id } })
+      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/`, { params: { question_id: questionItem.question_id } })
       setAnswerList(result.data.answers)
     }
     getAnswerList()
   }, [])
-
+  
   useEffect(() => {
     const fetchAnswer = async () => {
       const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
       setAnswer(result.data.characters)
     }
     fetchAnswer()
-    // setAnswer(answer1)
   }, [answerList])
 
-  questionItem.pictures.map((quesItem) =>
-    answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (ansItem.picture = quesItem.picture))
-  )
+  const getAnswer = () => useEffect(() => {
+    const fetchAnswer = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_API_HOST}/answers/${answerList[selectedAnswerNum].answer_id}/`)
+      setAnswer(result.data.characters)
+    }
+    fetchAnswer()
+  }, [answerList])
+  console.log(answerList)
+  console.log(answer)
+  console.log(selectedAnswerNum)
 
   const clickBackButton = () => {
     navigate(-2)
@@ -135,14 +60,22 @@ const AnswerDetail = () => {
   const clickPrevButton = () => {
     if (selectedAnswerNum > 0) {
       setSelectedAnswerNum(selectedAnswerNum - 1)
-      setAnswer(answerList[selectedAnswerNum])
+      getAnswer()
+      // setAnswer(answerList[selectedAnswerNum])
+      questionItem.pictures.map((quesItem) =>
+        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (ansItem.picture = quesItem.picture))
+      )
     }
   }
 
   const clickNextButton = () => {
     if (selectedAnswerNum < answerList.length) {
       setSelectedAnswerNum(selectedAnswerNum + 1)
-      setAnswer(answerList[selectedAnswerNum])
+      getAnswer()
+      // setAnswer(answerList[selectedAnswerNum])
+      questionItem.pictures.map((quesItem) =>
+        answer.map((ansItem) => ansItem.picture_id === quesItem.picture_id && (ansItem.picture = quesItem.picture))
+      )
     }
   }
 
@@ -176,7 +109,7 @@ const AnswerDetail = () => {
           {answer.length > 0 &&
             questionItem.pictures.map((picture, idx) => (
               <ImageListItem className={style.imgListItem} key={picture.picture_id} sx={{ margin: '0 0 -1px -1px' }}>
-                <Image className={style.imgList} src={picture.picture} style={{ width: '50%', margin: 'auto' }} />
+                <Image className={style.imgList} src={`data:image/png;base64,${picture.picture}`} style={{ width: '50%', margin: 'auto' }} />
                 <ImageListItemBar
                   position="below"
                   title={answer[idx].character_name}
@@ -194,7 +127,7 @@ const AnswerDetail = () => {
               </ImageListItem>
             ))}
         </ImageList>
-        {(selectedAnswerNum !== 0)
+        {(selectedAnswerNum !== answerList.length - 1)
           ? <button type="button" onClick={clickNextButton}>
               <img src={right} alt="next" />
               <Text text="次の回答へ" style={{ color: '#545454', fontSize: 'small' }} />
